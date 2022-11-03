b1df659b5fbb11084b39052393b97c908f65941c
Elena Donyakina <hellengoll@gmail.com>
2022-11-03 06:14:28 +0300

    corrected brain_even.py cod; add logic.py and even_game.py

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,25 @@
+
+import random
+
+
+START_NUMBER = 1
+END_NUMBER = 300
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_round_data():
+    random_number = random.randint(START_NUMBER, END_NUMBER)
+
+
+    if is_even(random_number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    
+
+    question = random_number
+    return question, correct_answer
+
+
+def is_even(random_number):
+    return random_number % 2 == 0
---
+++ brain_games/logic.py
@@ -0,0 +1,29 @@
+import prompt
+
+
+SCORE = 3
+
+
+def lounch(game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(game.DESCRIPTION)
+
+
+    index = 0
+    while index < SCORE:
+        question, correct_answer = game.get_round_data()
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+
+        if answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was "
+                  f"'{correct_answer}'.\nLet's try again, {name}!")
+            return
+        index += 1
+
+
+    print(f'Congratulations, {name}!')
