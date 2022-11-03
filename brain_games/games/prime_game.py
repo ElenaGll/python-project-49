2019cc7c4bea259959a3e806e6e17732749e7ae5
Elena Donyakina <hellengoll@gmail.com>
2022-11-04 01:29:57 +0300

    fixed bugs

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -15,10 +15,10 @@ def get_round_data():
 
 def is_prime(num):
     divisors = []
-    for i in range(1, num + 1):
+    for i in range(1, num):
         if num % i == 0:
             divisors.append(i)
-    if len(divisors) == 2:
+    if len(divisors) == 1:
         return True
     else:
         return False
