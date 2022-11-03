f6d4e390f2830b582fbd1066ef97057d74d50ab9
Elena Donyakina <hellengoll@gmail.com>
2022-11-03 09:26:57 +0300

    add new game: brain-calc

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,25 @@
+import random
+
+
+START_NUMBER = 1
+END_NUMBER = 50
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def get_round_data():
+    number_1 = random.randint(START_NUMBER, END_NUMBER)
+    number_2 = random.randint(START_NUMBER, END_NUMBER)
+    oper = random.choice(('+', '-', '*'))
+    
+    question = str(number_1) + ' ' + oper + ' ' + str(number_2)
+    correct_answer = str(calculate(number_1, number_2, oper))
+    return question, correct_answer
+
+
+def calculate(number_1, number_2, oper):
+    if oper == '+':
+        return number_1 + number_2
+    elif oper == '-':
+        return number_1 - number_2
+    elif oper == '*':
+        return number_1 * number_2
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+
+
+from brain_games.games import calc_game
+from brain_games.logic import lounch
+
+
+def main():
+    lounch(calc_game)  
+
+
+if __name__ == '__main__':
+    main()
