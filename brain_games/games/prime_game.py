d97dab020818f63f85506cfb0b63bdbd35b34860
Elena Donyakina <hellengoll@gmail.com>
2022-11-04 01:45:36 +0300

    fixed bugs

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -8,19 +8,21 @@ END_NUMBER = 301
 
 def get_round_data():
     question_number = random.randint(START_NUMBER, END_NUMBER)
+
     if is_prime(question_number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
+        
     return question_number, correct_answer
 
 
 def is_prime(num):
-    divisors = []
-    for i in range(1, num):
-        if num % i == 0:
-            divisors.append(i)
-    if len(divisors) == 1:
-        return True
-    else:
+    if num < 2:
         return False
+
+    for i in range(2, random_number):
+        if num % i == 0:
+            return False
+
+    return True
