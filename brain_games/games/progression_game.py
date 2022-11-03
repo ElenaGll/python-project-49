a48998502e90f0579882e068e5ec33ddf09bf405
Elena Donyakina <hellengoll@gmail.com>
2022-11-04 02:15:03 +0300

    fixed bugs in progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -2,12 +2,18 @@ import random
 
 
 DESCRIPTION = 'What number is missing in the progression?'
+MIN_LEN_PROGR = 5
+MAX_LEN_PROGR = 10
+MIN_START_NUM = 1
+MAX_START_NUM = 100
+MIN_STEP = 1
+MAX_STEP = 13
 
 
 def get_round_data():
-    len_progr = random.randint(5, 10)
-    start_num_progr = random.randint(1, 100)
-    step_progr = random.randint(1, 13)
+    len_progr = random.randint(MIN_LEN_PROGR, MAX_LEN_PROGR)
+    start_num_progr = random.randint(MIN_START_NUM, MAX_START_NUM)
+    step_progr = random.randint(MIN_STEP, MAX_STEP)
 
     i = 0
     progr = []
@@ -19,4 +25,4 @@ def get_round_data():
     index_missing_number = random.randint(0, len_progr - 1)
     missing_number = str(progr[index_missing_number])
     progr[index_missing_number] = '..'
-    return progr, missing_number
+    return ' '.join(map(str, progr)) , missing_number
